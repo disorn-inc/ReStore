fc503bf4c1fe21cb230d6aaaff85dc7e3449a8f1
disorn-inc <dirsorn1111@hotmail.com>
2021-11-27 01:51:29 +0700

    web API templete

---
+++ API/Data/Migrations/20211121101031_FixCreate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class FixCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStokck",
+                table: "Products",
+                newName: "QuantityInStock");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityInStokck");
+        }
+    }
+}
