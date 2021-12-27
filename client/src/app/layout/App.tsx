8b35ea496a03499c998dfac20dca65a02b9381d1
Disorn <disorn.1111@mail.kmutt.ac.th>
2021-12-27 10:59:51 +0700

    [ add ] axios and router

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,11 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import React, { useState } from 'react';
+import { Route } from 'react-router';
+import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import ContactPage from '../../features/contact/ContactPage';
+import HomePage from '../../features/home/HomePage';
 import Header from './Header';
 
 
@@ -25,7 +30,11 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-          <Catalog />
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/catalog' component={Catalog}/>
+          <Route path='/catalog/:id' component={ProductDetails}/>
+          <Route path='/about' component={AboutPage}/>
+          <Route path='/contract' component={ContactPage}/>
       </Container>
     </ThemeProvider>
   );
