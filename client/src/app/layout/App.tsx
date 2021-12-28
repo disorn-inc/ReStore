65a1f089860d6dd69ba5e79f6860498c1e35d798
Disorn <disorn.1111@mail.kmutt.ac.th>
2021-12-28 20:40:30 +0700

    [ add ] Error handling component and ainterception

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,12 +1,17 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import React, { useState } from 'react';
 import { Route } from 'react-router';
+import { ToastContainer } from 'react-toastify';
 import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
 import ProductDetails from '../../features/catalog/ProductDetails';
 import ContactPage from '../../features/contact/ContactPage';
 import HomePage from '../../features/home/HomePage';
 import Header from './Header';
+import 'react-toastify/dist/ReactToastify.css'
+import ServerError from '../errors/ServerError';
+import NotFound from '../errors/NotFound';
+import { Switch } from 'react-router-dom';
 
 
 function App() {
@@ -27,14 +32,19 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
+        <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/catalog' component={Catalog}/>
           <Route path='/catalog/:id' component={ProductDetails}/>
           <Route path='/about' component={AboutPage}/>
           <Route path='/contract' component={ContactPage}/>
+          <Route path='/server-error' component={ServerError}/>
+          <Route component={NotFound}/>
+        </Switch>
       </Container>
     </ThemeProvider>
   );
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -2,6 +2,8 @@ import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, T
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import agent from "../../app/api/agent";
+import NotFound from "../../app/errors/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 
 export default function ProductDetails() {
@@ -17,11 +19,11 @@ export default function ProductDetails() {
     }, [id]);
 
     if(loading) {
-        return <h3>Loading...</h3>
+        return <LoadingComponent message='Loading product...' />
     }
 
     if(!product) {
-        return <h3>Product not found</h3>
+        return <NotFound />
     }
 
     return (
