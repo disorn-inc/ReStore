dc734dc271db3a47c86a155533b3d5ba395e2ba9
Disorn <disorn.1111@mail.kmutt.ac.th>
2021-12-31 13:45:37 +0700

    [ fix ] refactor basket state to redux.

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -12,22 +12,24 @@ import {
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import agent from "../../app/api/agent";
-import useStoreContext from "../../app/context/StoreContext";
 import { Product } from "../../app/models/product";
+import { useAppDispatch } from "../../app/store/configureStore";
 import { currencyFormat } from "../../app/util/uttil";
+import { setBasket } from "../basket/basketSlice";
 
 interface Props {
   product: Product;
 }
 
 export default function ProductCard({ product }: Props) {
+  const dispatch = useAppDispatch();
+  
   const [loading, setLoding] = useState(false);
-  const {setBasket} = useStoreContext();
 
   function handleAddItem(productId: number) {
     setLoding(true);
     agent.Basket.addItem(productId)
-      .then(basket => setBasket(basket))
+      .then(basket => dispatch(setBasket(basket)))
       .catch(error => console.log(error))
       .finally(() => setLoding(false));
   }
