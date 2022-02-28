77922ee24a19dd970c96235feb3b962df5af8319
Disorn <disorn.1111@mail.kmutt.ac.th>
2022-02-28 20:32:43 +0700

    [ add ] http response extend to custom header.

---
+++ API/Extensions/HttpExtendsion.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using API.RequestHelpers;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Extensions
+{
+    public static class HttpExtendsion
+    {
+        public static void AddPaginationHeader(this HttpResponse response, MetaData metadata)
+        {
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(metadata, options: options));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
