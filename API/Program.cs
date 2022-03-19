acccdbe72efe85dac5a31f191872f6a559772b16
Disorn <disorn.1111@mail.kmutt.ac.th>
2022-03-20 01:34:46 +0700

    [ add ] identity fw and entity config.

--- API/Program.cs
+++ API/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,18 +16,19 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             // CreateHostBuilder(args).Build().Run();
             var host = CreateHostBuilder(args).Build();
             // add using instead of finally keyword
             using var scope = host.Services.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
             var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
             try
             {
-                context.Database.Migrate();
-                DbInitializer.Intitialize(context);
+                await context.Database.MigrateAsync();
+                await DbInitializer.Intitialize(context, userManager);
             } 
             catch (Exception ex)
             {
@@ -36,7 +39,7 @@ namespace API
             //     scope.Dispose();
             // }
             // "?" some thing about garbage collection
-            host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
